82333a9fca6087683a415cd974882215f85587e5
lyklee81-hue <lyklee81@gmail.com>
2025-11-25 16:34:13 +0900

    array

--- if.js
+++ if.js
@@ -21,15 +21,47 @@ if (score >= 60) {
 }
 
 console.log("if 종료");
-
-if (score >= 90) {
+/**
+ *   if (score >= 90) {
   console.log("A등급");
-} else if (score >= 70) {
+} else if (score >= 80) {
   console.log("B등급");
-} else if (score >= 60) {
+} else if (score >= 70) {
   console.log("C등급");
-} else if (score >= 50) {
+} else if (score >= 60) {
   console.log("C등급");
 } else {
   console.log("D등급");
 }
+   */
+
+//삼항연산자
+score > 60 ? pass() : fail();
+
+const isCheck = "abc" === "abc";
+console.log("🚀 ~ isCheck:", isCheck);
+
+const userInput = "";
+if (!userInput) {
+  // userInput의 값이 비어있는 경우
+  console.log("값을 입력해주세요");
+}
+
+// 배열
+// 재료 목록 데이터
+const ingredients = ["김치", "버섯", "계란", "양배추"];
+console.log("🚀 ~ ingredients:", ingredients);
+console.log("🚀 ~ ingredients:", ingredients);
+console.log("🚀 ~ ingredients:", ingredients[2]);
+console.log("ingredients", ingredients.length);
+
+const person = {
+  name: "이영규",
+  age: 45,
+  home: "부산시 연제구",
+  company: "부산시 진구",
+  isMale: true,
+};
+
+console.log("0번째 데이터", person["name"]);
+console.log("company", person.company);
